78cee46ca557c433f230c54e1619095cf6e7a14a
jsandicr <jsandicr16@gmail.com>
2024-09-23 11:21:14 -0600

    change port

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use('/auth', authRoutes);
 app.use('/expenses', expenseRoutes);
 app.use('/groups', groupRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
